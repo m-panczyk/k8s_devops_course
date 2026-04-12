8bbb20b2ef3db0079811ad9ebc59337676b0de4c
ialexbpl <spartan03311@gmail.com>
2026-04-12 13:52:01 +0200

    fix: enhance BASE value assignment and clean VM_ID extraction in network configuration script

--- set_network.sh
+++ set_network.sh
@@ -1,18 +1,21 @@
+# Ustaw numer fizycznego noda (1, 2, 3 lub 4)
 NODE_NUMBER=1 
 
-if [ $NODE_NUMBER -eq 1 ]; then BASE=0; fi
-if [ $NODE_NUMBER -eq 2 ]; then BASE=6; fi
-if [ $NODE_NUMBER -eq 3 ]; then BASE=12; fi
-if [ $NODE_NUMBER -eq 4 ]; then BASE=18; fi
+if [ $NODE_NUMBER -eq 1 ]; then BASE=0; fi  # OPOVMKUB01 -> 10.1.1.1
+if [ $NODE_NUMBER -eq 2 ]; then BASE=6; fi  # OPOVMKUB01 -> 10.1.1.7
+if [ $NODE_NUMBER -eq 3 ]; then BASE=12; fi # OPOVMKUB01 -> 10.1.1.13
+if [ $NODE_NUMBER -eq 4 ]; then BASE=18; fi # OPOVMKUB01 -> 10.1.1.19
 
 for VM in $(limactl list --format '{{.Name}}'); do
   echo "Konfiguruję sieć dla: $VM"
   
-  # Używamy \$, aby bash na Macu nie zamienił tego na pustkę, 
-  # zanim wyśle komendę do VM
   limactl shell $VM -- sudo bash -c "
+    # Wyciąga cyfry z końca nazwy (np. 01, 02)
     VM_ID=\$(hostname | grep -o '[0-9]\+$')
-    FINAL_IP=\$(( $BASE + VM_ID ))
+    # Usuwa wiodące zero, aby Bash nie pomyślał, że to liczba ósemkowa
+    VM_ID_CLEAN=\$((10#\$VM_ID))
+    
+    FINAL_IP=\$(( $BASE + VM_ID_CLEAN ))
     
     cat > /etc/netplan/99-bridged-static.yaml << EOF
 network:
